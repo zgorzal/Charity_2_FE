7c094054bf63b5848e57777ddcba7ed03c041628
zgorzal <zgorzalek.piotr@gmail.com>
2021-12-11 09:23:16 +0100

    go to section

--- src/mainPage/header/NavigatorHeader.js
+++ src/mainPage/header/NavigatorHeader.js
@@ -2,61 +2,68 @@ import React from "react";
 import Button from "../../component/Button";
 
 const NavigatorHeader = (props) => {
-  const StartButton = props.startButton;
-  const DonateButton = props.donateButton;
-  const donateFormIsActive = props.donateFormIsActive;
+    const StartButton = props.startButton;
+    const DonateButton = props.donateButton;
+    const donateFormIsActive = props.donateFormIsActive;
 
-  return (
-    <nav className="container container--70">
-      <ul className="nav--actions">
-        <li>
-          <Button name="Zaloguj" classList={["btn--small"]} />
-        </li>
-        <li>
-          <Button
-            name="Załóż konto"
-            classList={["btn--small btn--highlighted"]}
-          />
-        </li>
-      </ul>
+    return (
+        <nav className="container container--70">
+            <ul className="nav--actions">
+                <li>
+                    <Button name="Zaloguj" classList={["btn--small"]}/>
+                </li>
+                <li>
+                    <Button
+                        name="Załóż konto"
+                        classList={["btn--small btn--highlighted"]}
+                    />
+                </li>
+            </ul>
 
-      <ul>
-        {donateFormIsActive && (
-          <li>
-            <Button name="Start" handleClick={StartButton} />
-          </li>
-        )}
+            <ul>
+                {donateFormIsActive && (
+                    <li>
+                        <Button name="Start" handleClick={StartButton}/>
+                    </li>
+                )}
 
-        {!donateFormIsActive && (
-          <li>
-            <Button name="O co chodzi?" />
-          </li>
-        )}
+                {!donateFormIsActive && (
+                    <div onClick={() => window.location.replace("/#steps")}>
+                        <li>
+                            <Button name="O co chodzi?"/>
+                        </li>
+                    </div>
+                )}
 
-        {!donateFormIsActive && (
-          <li>
-            <Button name="O nas" />
-          </li>
-        )}
+                {!donateFormIsActive && (
+                    <div onClick={() => window.location.replace("/#about-us")}>
+                        <li>
+                            <Button name="O nas"/>
+                        </li>
+                    </div>
+                )}
 
-        {!donateFormIsActive && (
-          <li>
-            <Button name="Fundacje i organizacje" />
-          </li>
-        )}
+                {!donateFormIsActive && (
+                    <div onClick={() => window.location.replace("/#help")}>
+                        <li>
+                            <Button name="Fundacje i organizacje"/>
+                        </li>
+                    </div>
+                )}
 
-        {!donateFormIsActive && (
-          <li>
-            <Button name="Przekaż dary" handleClick={DonateButton} />
-          </li>
-        )}
-
-        <li>
-          <Button name="Kontakt" />
-        </li>
-      </ul>
-    </nav>
-  );
+                {!donateFormIsActive && (
+                    <li>
+                        <Button name="Przekaż dary" handleClick={DonateButton}/>
+                    </li>
+                )}
+                <div onClick={() => window.location.replace("/#contact")}>
+                    <li>
+                        <Button name="Kontakt"/>
+                    </li>
+                </div>
+            </ul>
+        </nav>
+    );
 };
 
 export default NavigatorHeader;
